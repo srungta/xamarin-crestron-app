471e426eb1b90cd2b32fbd2bdea0a9e944f5bb2f
Shaswat Rungta <shrung@microsoft.com>
2017-07-21 22:28:55 +0530

    Added Crestron Activity View Model

--- CrestronApp/CrestronApp/Models/CrestronRoom.cs
+++ CrestronApp/CrestronApp/Models/CrestronRoom.cs
@@ -7,17 +7,49 @@ namespace CrestronApp.Models
     public class CrestronRoom : BaseDataObject
     {
         string name = string.Empty;
+        string ipAddress = string.Empty;
+        public long ipId = 7;
+        public string userName = string.Empty;
+        public string password = string.Empty;
+        public int port = 12345;
+        public bool useSsl = false;
+
         public string Name
         {
             get { return name; }
             set { SetProperty(ref name, value); }
         }
 
-        string ipAddress = string.Empty;
         public string IpAddress
         {
             get { return ipAddress; }
             set { SetProperty(ref ipAddress, value); }
+        }
+
+        public long IpId
+        {
+            get { return ipId; }
+            set { SetProperty(ref ipId, value); }
+        }
+        public string UserName
+        {
+            get { return ipAddress; }
+            set { SetProperty(ref ipAddress, value); }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+        public int Port
+        {
+            get { return port; }
+            set { SetProperty(ref port, value); }
+        }
+        public bool UseSsl
+        {
+            get { return useSsl; }
+            set { SetProperty(ref useSsl, value); }
         }
     }
 }
---
+++ CrestronApp/CrestronApp/ViewModels/CrestronRoomActivityViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Crestron.ActiveCNX;
+using CrestronApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrestronApp.ViewModels
+{
+    public class CrestronRoomActivityViewModel : BaseViewModel
+    {
+        public ActiveCNX Device;
+
+        public CrestronRoomActivityViewModel(CrestronRoom room = null)
+        {
+            Title = room.Name;
+            Device = new ActiveCNX(room.IpAddress, room.IpId, room.UserName, room.Password, room.Port, room.UseSsl);
+            Device.onConnect += Device_onConnect;
+            Device.onDigital += Device_onDigital;
+            Device.onAnalog += Device_onAnalog;
+            Device.onDisconnect += Device_onDisconnect;
+            Device.onError += Device_onError;
+        }
+
+        private void Device_onError(object sender, ActiveCNXErrorEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Device_onDisconnect(object sender, ActiveCNXConnectionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Device_onAnalog(object sender, ActiveCNXEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Device_onDigital(object sender, ActiveCNXEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Device_onConnect(object sender, ActiveCNXConnectionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeviceConnect()
+        {
+            Device.Connect();
+        }
+
+        public void DeviceDisconnect()
+        {
+            Device.Disconnect();
+        }
+    }
+}
